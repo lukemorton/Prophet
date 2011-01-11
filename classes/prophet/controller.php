dc1e9804bf9087fadec749d0196906bdf1d6e133
Luke Morton <lukemorton.designs@gmail.com>
2011-01-11 01:20:59 +0000

    Clean up of whitespace

--- classes/prophet/controller.php
+++ classes/prophet/controller.php
@@ -10,71 +10,71 @@ class Prophet_Controller extends Kohana_Controller {
 	
 	/**
 	 * @var  string  The view class
-     * @protected
+	 * @protected
 	 */
 	protected $_view_class = 'View';
-    
+	
 	/**
 	 * @var  array  Define viewless actions here
-     * @protected
+	 * @protected
 	 */
 	protected $_viewless = array();
-    
+	
 	/**
 	 * @var  mixed  Holds the current view instance
-     * @protected
+	 * @protected
 	 */
 	protected $_view = NULL;
-    
-    /**
-     * Magic get method used for lazy loading the view so if you
-     * don't call the view, it won't be loaded.
-     *
-     * @param  string  Variable name
-     */
-    public function __get($name)
-    {
-        if ($name === 'view')
-        {
-            if ($this->_view === NULL && ! in_array($this->request->action(), $this->_viewless))
-            {
-                $view_parts = array();
-                
-                foreach (array('directory', 'controller', 'action') as $_part)
-                {
-                    $part = call_user_func(array($this->request, $_part));
-                    
-                    if ( ! empty($part))
-                    {
-                        $view_parts[] = $part;
-                    }
-                }
-                
-                // Build the view location
-                $view_location = implode('/', $view_parts);
-                
-                // Load the view using chosen class
-                $this->_view = call_user_func($this->_view_class.'::factory', $view_location);
-            }
-            
-            return $this->_view;
-        }
-    }
-    
-    /**
-     * Magic set method
-     *
-     * @param   string  Name
-     * @param   mixed   Value
-     * @return  void
-     */
-    public function __set($name, $value)
-    {
-        if ($name === 'view')
-        {
-            $this->_view = $value;
-        }
-    }
+	
+	/**
+	 * Magic get method used for lazy loading the view so if you
+	 * don't call the view, it won't be loaded.
+	 *
+	 * @param  string  Variable name
+	 */
+	public function __get($name)
+	{
+		if ($name === 'view')
+		{
+			if ($this->_view === NULL && ! in_array($this->request->action(), $this->_viewless))
+			{
+				$view_parts = array();
+				
+				foreach (array('directory', 'controller', 'action') as $_part)
+				{
+					$part = call_user_func(array($this->request, $_part));
+					
+					if ( ! empty($part))
+					{
+						$view_parts[] = $part;
+					}
+				}
+				
+				// Build the view location
+				$view_location = implode('/', $view_parts);
+				
+				// Load the view using chosen class
+				$this->_view = call_user_func($this->_view_class.'::factory', $view_location);
+			}
+			
+			return $this->_view;
+		}
+	}
+	
+	/**
+	 * Magic set method
+	 *
+	 * @param   string  Name
+	 * @param   mixed   Value
+	 * @return  void
+	 */
+	public function __set($name, $value)
+	{
+		if ($name === 'view')
+		{
+			$this->_view = $value;
+		}
+	}
 	
 	/**
 	 * The after method turns the view into a response
--- init.php
+++ init.php
@@ -2,19 +2,19 @@
 
 if (Kohana::$errors)
 {
-    // Override Kohana exception handler
-    set_exception_handler(array('prophet', 'exception_handler'));
+	// Override Kohana exception handler
+	set_exception_handler(array('prophet', 'exception_handler'));
 }
 
 // Error Route for internal error requests
 Route::set('prophet_error', 'prophet_error/<action>(/<message>)', array('action' => '[0-9]{3}', 'message' => '.*'))
-    ->defaults(array(
-        'controller' => 'error',
-    ));
+	->defaults(array(
+		'controller' => 'error',
+	));
 
 // Catch All Route (404 Response)
 Route::set('prophet_catchall', '<catchall>', array('catchall' => '.*'))
-    ->defaults(array(
-        'controller' => 'error',
-        'action'     => '404',
-    ));
+	->defaults(array(
+		'controller' => 'error',
+		'action'	 => '404',
+	));
